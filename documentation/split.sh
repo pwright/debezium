c784f5f80d75e59d2555fc5352b8a2f34c5484ca
Paul Wright <pwright@redhat.com>
2022-06-27 12:24:26 +0100

    add split

---
+++ documentation/split.sh
@@ -0,0 +1,3 @@
+nebel split -a ./attributes.adoc --conditions product --category-prefix debezium --legacybasedir ./modules/ROOT/pages ./modules/ROOT/pages/connectors/oracle.adoc
+
+nebel split -a ./attributes.adoc --conditions product --category-prefix debezium --legacybasedir ./modules/ROOT/pages ./modules/ROOT/pages/connectors/db2.adoc
